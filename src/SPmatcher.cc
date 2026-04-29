251324f7eab3b9f3ae0a8ed1282d4bdb9c3857eb
ysfmerttyldz <ysfmerttyldz@gmail.com>
2026-04-29 15:29:04 +0300

    edited

--- src/SPmatcher.cc
+++ src/SPmatcher.cc
@@ -1,22 +1,3 @@
-/**
-* This file is part of SP-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/SP_SLAM2>
-*
-* SP-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* SP-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with SP-SLAM2. If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #include "SPmatcher.h"
 #include "LightGlue.h"
